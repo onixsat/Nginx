89691e133a064bf6222c85537cfc3bdda96922f6
onixsat <163943278+onixsat@users.noreply.github.com>
2026-02-26 21:31:12 +0000

    Configure firewall and update service management
    Added firewall rules for Nginx and OpenSSH, enabled UFW, and updated service restart commands to use sudo.

--- config.sh
+++ config.sh
@@ -14,18 +14,24 @@ chown -R www-data:www-data /var/www/stream
 chown -R www-data:www-data /var/www/html
 chmod -R 777 /var/www/stream/*
 chmod -R 777 /var/www/html/*
+ufw allow 'Nginx Full'
 
+ufw allow OpenSSH
+ufw --force enable
+sudo apt-get install iptables-persistent
+sudo iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT
+sudo iptables -I INPUT 1 -p tcp --dport 8443 -j ACCEPT
 
 echo "Testing Nginx configuration..."
 nginx -t
 
 # Restart services
 echo "Restarting services..."
-systemctl restart nginx
-systemctl restart php8.1-fpm
+sudo systemctl restart nginx
+sudo systemctl restart php8.1-fpm
 
 # Start and enable Nginx UI
-systemctl start nginx-ui
-systemctl enable nginx-ui
- #   systemctl restart nginx-ui
+sudo systemctl start nginx-ui
+sudo systemctl enable nginx-ui
+#systemctl restart nginx-ui
 
